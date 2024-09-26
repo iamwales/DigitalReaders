61c23f05db5729202cbbf08cb5e4d74ad59ee835
iamwales01 <aolawale@360alumni.com>
2024-09-26 10:09:09 +0100

    authentication service

--- book-socials/src/main/java/com/wales/book_socials/auth/AuthenticationService.java
+++ book-socials/src/main/java/com/wales/book_socials/auth/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.wales.book_socials.auth;
 import com.wales.book_socials.email.EmailService;
 import com.wales.book_socials.email.EmailTemplateName;
 import com.wales.book_socials.role.RoleRepository;
+import com.wales.book_socials.security.JwtService;
 import com.wales.book_socials.user.Token;
 import com.wales.book_socials.user.TokenRepository;
 import com.wales.book_socials.user.User;
@@ -10,11 +11,14 @@ import com.wales.book_socials.user.UserRepository;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -26,6 +30,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final EmailService emailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Value("${application.mailing.frontend.activation-url}")
     private String activationUrl;
@@ -93,4 +99,18 @@ public class AuthenticationService {
         return codeBuilder.toString();
     }
 
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var claims = new HashMap<String, Object>();
+        var user = ((User)auth.getPrincipal());
+        claims.put("fullName", user.getFullName());
+        var jwtToken = jwtService.generateToken(claims, user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
 }
